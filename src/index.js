12dea7c8267dff7db7e23d128c5f799eb2a45553
Rununu Games <ngugikerei@gmail.com>
2023-10-14 14:08:36 +0300

    Styling Elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,21 +58,36 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
-      <h1>Our Menu</h1>
+    <main className="menu">
+      <h2>Our Menu</h2>
       <Pizza />
-    </div>
+      <Pizza />
+      <Pizza />
+    </main>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
-    <footer> {new Date().toLocaleTimeString()}. We're currently open!</footer>
+    <footer className="footer">
+      {' '}
+      {new Date().toLocaleTimeString()}. We're currently open!
+    </footer>
   );
 }
 
@@ -79,7 +95,7 @@ export default function Pizza() {
   return (
     <div className="pizzaDisp">
       <img src="pizzas/spinaci.jpg" alt="pizzaSpinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
