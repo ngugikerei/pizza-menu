ed8054064cfa26e9c8733b61529b6ae93191f8ac
Rununu Games <ngugikerei@gmail.com>
2023-10-21 07:50:23 +0300

    Rendering pizzas on UI

--- src/index.js
+++ src/index.js
@@ -6,42 +6,42 @@ const pizzaData = [
   {
     name: 'Focaccia',
     ingredients: 'Bread with italian olive oil and rosemary',
-    price: 6,
+    price: 600,
     photoName: 'pizzas/focaccia.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Margherita',
     ingredients: 'Tomato and mozarella',
-    price: 10,
+    price: 1000,
     photoName: 'pizzas/margherita.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Spinaci',
     ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-    price: 12,
+    price: 1200,
     photoName: 'pizzas/spinaci.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Funghi',
     ingredients: 'Tomato, mozarella, mushrooms, and onion',
-    price: 12,
+    price: 1200,
     photoName: 'pizzas/funghi.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Salamino',
     ingredients: 'Tomato, mozarella, and pepperoni',
-    price: 15,
+    price: 1500,
     photoName: 'pizzas/salamino.jpg',
     soldOut: true,
   },
   {
     name: 'Pizza Prosciutto',
     ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-    price: 18,
+    price: 1800,
     photoName: 'pizzas/prosciutto.jpg',
     soldOut: false,
   },
@@ -67,37 +67,52 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const arrayLength = pizzas.length;
+
   return (
     <main className="menu">
+      {arrayLength > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>Ksh {props.pizzaObj.price}</span>
+      </div>
+    </li>
   );
 }
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 7;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
-      {' '}
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {/* {new Date().toLocaleTimeString()}. We're currently open! */}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
-  );
-}
-
-export default function Pizza() {
-  return (
-    <div className="pizzaDisp">
-      <img src="pizzas/spinaci.jpg" alt="pizzaSpinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
